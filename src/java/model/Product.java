961c19a46b0769f3228399c8c40c4d9dedeb6e76
vuduy1309 <duyakali12@gmail.com>
2024-06-20 20:55:09 +0700

    update product model

--- src/java/model/Product.java
+++ src/java/model/Product.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.sql.Date;
 
-/**
- *
- * @author vanmi
- */
+
 public class Product {
 
     private String id, name;
